fe931b2bde0dfb154d72dc5b7fa1c51af7368600
salindae25 <salindae25@gmail.com>
2022-11-02 19:25:27 +0530

    initial commit

---
+++ lua/base.lua
@@ -0,0 +1,47 @@
+vim.cmd('autocmd!')
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.wo.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'powershell'
+vim.opt.backupskip = '/tmp/*./private/tmp/*'
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.si = true
+vim.opt.wrap = false
+vim.opt.path:append { '**' }
+vim.opt.wildignore:append { '*/node_modules/*'}
+
+vim.opt.foldlevel=20
+vim.opt.foldmethod='indent'
+--vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+vim.opt.mouse='inv' -- mouse active on i = insert n=normal v=visual mode
+
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
---
+++ lua/maps.lua
@@ -0,0 +1,38 @@
+local keymap = vim.keymap
+
+--keymap.set('n','x','_x')
+-- Goto normal mode
+
+keymap.set('i','jk','<Esc>')
+keymap.set('i','kj','<Esc>')
+
+keymap.set('v','q','<Esc>')
+--quit window
+keymap.set('n',';q',':q<Return>')
+
+-- Save file
+keymap.set('',';s',':w<Return>')
+
+-- select all
+keymap.set('n', '<C-a>','gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>')
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('n','<Tab>',':tabnext<Return>')
+
+keymap.set('n','s<left>','<C-w>h')
+keymap.set('n','s<up>','<C-w>k')
+keymap.set('n','s<down>','<C-w>j')
+keymap.set('n','s<right>','<C-w>l')
+
+keymap.set('n','sh','<C-w>h')
+keymap.set('n','sk','<C-w>k')
+keymap.set('n','sj','<C-w>j')
+keymap.set('n','sl','<C-w>l')
+
+-- terminal
+keymap.set('t','jk','<C-\\><C-n>')
